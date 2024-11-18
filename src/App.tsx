be76b0150121d3965323ea4f098c05455e46742e
cad3nh <hoov9753@kettering.edu>
2024-11-17 22:05:03 -0500

    undid everything

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,41 @@ const client = generateClient<Schema>();
 function App() {
   const { user, signOut } = useAuthenticator();
  // const { signOut } = useAuthenticator();
-    
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+  
   return (
     <main>
-      <button>Test</button>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li           
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+        Undid everything
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+        <button onClick={signOut}>Sign out</button>
+      </div>
     </main>
   );
 }
