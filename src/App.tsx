908f57438dcbb7070a27dbceaa0bcaec5848da45
cad3nh <hoov9753@kettering.edu>
2024-11-17 21:56:05 -0500

    Testing from scratch

--- src/App.tsx
+++ src/App.tsx
@@ -4,49 +4,15 @@ import type { Schema } from "../amplify/data/resource";
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
 
-import UserProfile from './UserProfile.tsx';
-
 const client = generateClient<Schema>();
 
 function App() {
   const { user, signOut } = useAuthenticator();
  // const { signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-  
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-  
+    
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li           
-            onClick={() => deleteTodo(todo.id)}
-            key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        if this text is showing then a new page should be available, there just isn't a link yet
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-        <button onClick={signOut}>Sign out</button>
-      </div>
+      <button>Test</button>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import App from './App.tsx';
-import UserProfile from './UserProfile.tsx'
 import outputs from '../amplify_outputs.json';
 import './index.css';
 import '@aws-amplify/ui-react/styles.css';
